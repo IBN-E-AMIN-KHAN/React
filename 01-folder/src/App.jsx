00b18113a2d99832a42b44cd1759a1d5403f326c
IBN-E-AMIN-KHAN <binyameenkhn03@outlook.com>
2026-09-09 13:59:47 +0500

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>This is my first line to React js</p>
+      <p>This is my phase 3 of learning App development</p>
+    </div>
+  );
+};
+
+export default App;
