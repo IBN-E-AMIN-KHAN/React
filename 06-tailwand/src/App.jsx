acf4a77662a979f5749a577516d1c404d44d559a
IBN-E-AMIN-KHAN <binyameenkhn03@outlook.com>
2026-09-12 15:48:17 +0500

    tailwand done

---
+++ 06-tailwand/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      {" "}
+      <div className="bg-blue-500 text-white p-4">App</div>
+      <div className="bg-red-500 text-white p-4 mt-4">This is tailwind tutorial</div>
+    </>
+  );
+};
+export default App;
